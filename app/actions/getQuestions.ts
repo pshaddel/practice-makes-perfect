bd3a5b79904aff2d199f83e38fb73cdce4ba2252
Poorshad <poorshad.shaddel@redbull.com>
2025-01-24 22:48:25 +0100

    more realistic case

--- app/actions/getQuestions.ts
+++ app/actions/getQuestions.ts
@@ -14,12 +14,18 @@ interface Question {
     tags: string[];
 }
 
-export async function getQuestions(tags: string[] = []): Promise<Question[]> {
-
+export async function getQuestions({
+    tags = [],
+    page = 1
+}: {
+    tags: string[];
+    page?: number;
+}): Promise<Question[]> {
     // Filter questions based on the tags
+    // sleep(1000);
+    await new Promise((resolve) => setTimeout(resolve, 500));
     const filteredQuestions = questions.filter((question: Question) => {
         return tags.every((tag) => question.tags.includes(tag));
     });
-
     return Promise.resolve(filteredQuestions);
 }
