d3b7a90cc1f3b7b5c07431aca7474247a4e4dfa7
Poorshad <poorshad.shaddel@redbull.com>
2025-01-24 22:48:00 +0100

    use link

--- app/components/StartButton.tsx
+++ app/components/StartButton.tsx
@@ -3,12 +3,16 @@ import React, { Suspense } from 'react';
 import { ArrowRight } from 'lucide-react';
 import { ReadonlyURLSearchParams, useRouter, useSearchParams } from 'next/navigation';
 import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
+import Link from 'next/link';
 
 export function goToQuestionsList(searchParams: ReadonlyURLSearchParams, router: AppRouterInstance) {
   // get previous URL to keep the url params
   const params = searchParams.get('tags');
-  const url = params ? `/questions?tags=${params}` : '/questions';
-  router.push(url);
+  //  const url = params ? `/questions?tags=${params}` : '/questions';
+  const url = '/questions' + (params ? `?tags=${params}` : '');
+
+  return url
+  // router.push(url);
 };
 
 export function Start() {
@@ -17,11 +21,12 @@ export function Start() {
   return (
     <button
       type='button'
-      onClick={() => goToQuestionsList(searchParams, router)}
       className="inline-flex items-center gap-2 px-8 py-4 bg-blue-600 text-white rounded-xl font-medium hover:bg-blue-700 transition-colors duration-200 group"
     >
-      Start Practice
-      <ArrowRight className="w-4 h-4 group-hover:translate-x-1 transition-transform duration-200" />
+      <Link href={goToQuestionsList(searchParams, router)} prefetch={true}>
+        Start Practice
+        <ArrowRight className="w-4 h-4 group-hover:translate-x-1 transition-transform duration-200" />
+      </Link>
     </button>
   );
 }
